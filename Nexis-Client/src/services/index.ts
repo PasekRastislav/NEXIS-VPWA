1a1df62c014075c453e55ae0898196954df59841
Kristián Červenka <80548418+K1ko@users.noreply.github.com>
2024-12-08 20:33:20 +0100

    Delete Nexis-Client directory

--- Nexis-Client/src/services/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as authManager } from './AuthManager'
-export { default as authService } from './AuthService'
-export { default as channelService } from './ChannelService'
-export { default as activityService } from './ActivityService'
